f6c4a753aed61608dfca741a69973696b03d3dce
Mark Harvilla <mharvill@tenant.pas.idealab.com>
2016-01-25 18:10:00 -0800

    added password reset view controller, doesn't yet make a call to the backend, will do next

---
+++ IceBreaker/IceBreaker/ForgotPasswordViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ForgotPasswordViewController.swift
+//  IceBreaker
+//
+//  Created by Mark Harvilla on 1/25/16.
+//  Copyright © 2016 Christopher Tischuk. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func exitButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
